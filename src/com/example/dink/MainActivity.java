ad79104abc66611fff61ef15f036a22539647b06
Lucas <lucas@laragno-MOV.(none)>
2013-10-12 11:53:18 -0300

    added support for input strings

--- src/com/example/dink/MainActivity.java
+++ src/com/example/dink/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.dink;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.sax.TextElementListener;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 
 	private ImageButton searchButton;
 	private EditText songField;
@@ -20,6 +22,7 @@ public class MainActivity extends Activity {
 		searchButton = (ImageButton) findViewById(R.id.imageButton1);
 		songField = (EditText) findViewById(R.id.editText1);
 		bandField = (EditText) findViewById(R.id.EditText01);
+		//searchButton.setOnClickListener(this);
 		setContentView(R.layout.activity_main);
 	}
 
@@ -28,6 +31,15 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		String songName = songField.getText().toString();
+		String bandName = bandField.getText().toString();
+		songName.replace(' ', '+');
+		bandName.replace(' ', '+');
+		
 	}
 
 }
